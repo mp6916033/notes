78d9358f298900450ce48d4f5c8b51e83cdd6587
Manas Pandey <manaspandey227@gmail.com>
2021-08-28 13:15:00 +0530

    notes app

---
+++ app/src/main/java/com/example/notes/add_note_menu.java
@@ -0,0 +1,14 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_note_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note_menu);
+    }
+}
